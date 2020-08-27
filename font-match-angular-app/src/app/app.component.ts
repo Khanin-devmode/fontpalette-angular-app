819887aa25dc3f3a050dcb51ffa9f581da3547a9
Khanin Chomphusri <Khaninz3779@gmail.com>
2020-08-27 16:02:24 +0700

    navigation ui

--- font-match-angular-app/src/app/app.component.ts
+++ font-match-angular-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  constructor(private router:Router) {
 
+  }
+
+  isActiveRoute(route:string):boolean{
+    return this.router.isActive(route,false);
+  }
 
 }
